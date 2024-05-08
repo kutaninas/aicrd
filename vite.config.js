5be7bfe6361639ad03d7a85042df44b7b6aac558
Alexandra Kutanina <alexandra.kutanina@bain.com>
2024-05-08 14:31:10 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  base: '/aicrd/', // Adjust this to match your repository path
+  plugins: [svelte()]
 })
